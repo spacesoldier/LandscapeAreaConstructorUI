fc119fa607066458b4b23e6ca72d6a1103487d9e
Vyacheslav Soldatov <kibersoldier@gmail.com>
2024-06-22 14:02:17 +0400

    loading project lists

--- src/components/constructor/project/ProjectList.js
+++ src/components/constructor/project/ProjectList.js
@@ -1,32 +1,89 @@
-import {Button, Card, CardBody, CardFooter, Typography} from "@material-tailwind/react";
+import {Button, Card, CardBody, CardFooter, CardHeader, Typography} from "@material-tailwind/react";
 import {FaPlus} from "react-icons/fa6";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 function ProjectList({stageUpdateSink, username}){
 
-    return (
-        <Card className="mt-6 w-96">
-            <CardBody>
-                <Typography variant="h5" color="blue-gray" className="mb-2">
-                    Projects
-                </Typography>
-                <Typography>
-                    all existing projects for the user {username}
-                </Typography>
-                <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+    const [projects,setProjects] = useState([])
 
-                </ul>
-            </CardBody>
-            <CardFooter className="pt-0">
-                <Button
-                    className="inline-flex items-center"
-                    onClick={() => stageUpdateSink("new-project")}
-                >
-                    <FaPlus className="w-4 h-4"/>
-                    <span className="pl-4">New Project</span>
-                </Button>
-            </CardFooter>
-        </Card>
+
+    const retrieveProjectsForUser = async () =>
+    {
+        const prj_rersponse = await axios.get(`http://localhost:8000/maf/projects/${username}`);
+
+        const {data} = prj_rersponse;
+
+        console.log(data);
+
+        const existing_projects = [];
+
+        while(projects.length > 0){
+            projects.pop()
+        }
+
+        data.projects.forEach(
+            (recv_project) => {
+                projects.push(recv_project);
+            }
+        );
+        console.log(projects);
+
+    }
+
+    useEffect(() => {
+        retrieveProjectsForUser();
+    })
+
+    return (
+        <div className="flex">
+            <Card className="mt-6 w-screen">
+                <CardBody>
+                    <Typography variant="h5" color="blue-gray" className="mb-2">
+                        Проекты пользователя {username}
+                    </Typography>
+                    <hr className="h-px" />
+                    <div className="grid grid-cols-4 h-full pt-8" >
+                        <div >
+                            <Button
+                                variant="outlined"
+                                className="h-40 w-80 inline-flex items-center border-4 border-gray-600"
+                                onClick={() => stageUpdateSink("new-project")}
+                            >
+                                <FaPlus className="w-8 h-8"/>
+                                <span className="pl-16">
+                                    <Typography variant="h5" color="blue-gray">
+                                        Начать новый
+                                    </Typography>
+                                </span>
+                            </Button>
+                        </div>
+                            {projects.map(project => (
+                                <Card key={project.project_id} className="h-40 w-80">
+                                    <CardBody>
+                                        <Typography>
+                                            <span>{project.project_id}</span>
+                                            <hr className="h-px" />
+                                            <span>{project.project_name}</span>
+                                        </Typography>
+                                    </CardBody>
+                                    <CardFooter>
+
+                                    </CardFooter>
+                                </Card>
+                            ))}
+
+                    </div>
+                </CardBody>
+                <CardFooter className="pt-0">
+
+                </CardFooter>
+            </Card>
+        </div>
+
+
+
+
     );
 }
 
