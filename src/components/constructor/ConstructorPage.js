cfb212c6245bbed07baa3d7c56a7cc4b6a9b0f2c
Vyacheslav Soldatov <kibersoldier@gmail.com>
2024-06-22 04:29:17 +0400

    start building the constructor UI

---
+++ src/components/constructor/ConstructorPage.js
@@ -0,0 +1,38 @@
+import React from "react";
+import SimpleCard from "./SimpleCard";
+import {FeaturedImageGallery} from "./ImageGallery";
+import {SidebarWithCta} from "./SidebarWithCta";
+import {SystemNavbar} from "./SystemNavbar";
+// import { useKeycloak } from "@react-keycloak/web";
+
+function ConstructorPage(){
+
+    // const { keycloak, initialized } = useKeycloak();
+
+    return (
+        <div className="App">
+            <SystemNavbar />
+
+            <div class="container">
+                <h1 className="bg-slate-500 text-black text-center">Тут будет список проектов пользователя</h1>
+            </div>
+            <div class="gap-8 columns-3">
+                <SimpleCard />
+                <SimpleCard />
+                <SimpleCard />
+                <SimpleCard />
+                <SimpleCard />
+                <SimpleCard />
+                <SimpleCard />
+                <SimpleCard />
+                <SimpleCard />
+            </div>
+            <div class="container content-center">
+                <FeaturedImageGallery />
+            </div>
+            <SidebarWithCta />
+        </div>
+    );
+}
+
+export default ConstructorPage
